669bc7f5aca3b353dee4190f00dafd29b32ec049
Pansera <lucaspansera12345@gmail.com>
2025-11-06 22:03:17 -0300

    06/11

--- the-moviebox-react/src/cases/components/movie-content.tsx
+++ the-moviebox-react/src/cases/components/movie-content.tsx
@@ -1,17 +1,31 @@
 import { useEffect, useState } from "react"
-import type { MovieDTO } from "../services/movie.service"
+import { MovieService, type MovieDTO } from "../services/movie.service"
+import { MovieCard } from "./movie-card";
 
-export function MovieContent(){
+export function MovieContent() {
 
-    const [movies, setMovies] = useState<MovieDTO[]>([])
+    const [movies, setMovies] = useState<MovieDTO[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false)
 
-    useEffect(()=> {}, []);
 
-    return(
-        <section 
-            className="bg-[#1c1c1c] p-8 grid grid-cols-[repeat(auto-fill, minmax(200px, 1fr))] gap-8 max-w-6xl mx-auto"
-        >
-            <p>{JSON.stringify(movies)}</p>
-        </section>
+    useEffect(() => {
+        setIsLoading(true);
+        MovieService.list().then((result) => {
+            setMovies(result)
+        }).finally(() => setIsLoading(false))
+    }, []);
+
+    return (
+        isLoading ? (
+        <div className="bg-[#1c1c1c] flex justify-center items-center h-40">
+                <p className="text-2xl text-center text-white">Carregando...</p>
+            </div>        ) :
+            <section
+                className="bg-[#1c1c1c] p-8 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-8 max-w-6xl mx-auto"
+            >
+                {movies.map((movie, index) => (
+                    <MovieCard key={index} movie={movie} />
+                ))}
+            </section>
     )
 }
