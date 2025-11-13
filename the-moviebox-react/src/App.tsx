8ec70a5ddee1dae703f35a1f0db61dbdd7a0f97e
Pansera <lucaspansera12345@gmail.com>
2025-11-13 14:48:20 -0300

    TrblhoWebHighlitgh

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,22 +1,23 @@
-import { Header } from "./componentes/layout/header"
-import { Footer } from "./componentes/layout/footer"
-import { Navbar } from "./componentes/layout/navbar"
-import { MovieService } from "./cases/services/movie.service"
-import { MovieContent } from "./cases/components/movie-content"
+import { Header } from "./componentes/layout/header";
+import { Footer } from "./componentes/layout/footer";
+import { Navbar } from "./componentes/layout/navbar";
+import { MovieService } from "./cases/services/movie.service";
+import { MovieContent } from "./cases/components/movie-content";
+import { Highlight } from "./componentes/layout/highligth";
 
 function App() {
- 
-
   return (
     <>
-     <Header />
-     <main>
-      <Navbar/>
-      <MovieContent/>
-     </main>
-     <Footer/>
+      <Header />
+      <main>
+        <Highlight />
+
+        <Navbar />
+        <MovieContent />
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
